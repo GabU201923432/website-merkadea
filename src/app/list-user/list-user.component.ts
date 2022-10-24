d7a4c6c9539944b15e181d45acaca8fb0cfc9005
GabU201923432 <102437264+GabU201923432@users.noreply.github.com>
2022-10-24 14:53:33 -0500

    cambios con errores

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { User } from '../models/tusers';
+import { HttpDataService } from '../services/http-data.service';
 
 @Component({
   selector: 'app-list-user',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListUserComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'store',
+    'actions',
+  ];
+
+  dataSource = new MatTableDataSource<User>();
+
+  User!:User[];
+
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  constructor(
+    private httpData: HttpDataService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
+    this.getUsers();
+
+  ngOnInit(): void {
+
+    applyFilter(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
   }
 
 }
