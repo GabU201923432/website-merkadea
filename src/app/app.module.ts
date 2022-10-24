512d07d92077132ea6d93497b5979416c17bddee
GabU201923432 <102437264+GabU201923432@users.noreply.github.com>
2022-10-24 13:17:19 -0500

    Agregación de nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { AddEditComponent } from './add-edit/add-edit.component';
+import { HomeComponent } from './home/home.component';
+import { ListUserComponent } from './list-user/list-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddUserComponent,
+    AddEditComponent,
+    HomeComponent,
+    ListUserComponent
   ],
   imports: [
     BrowserModule,
